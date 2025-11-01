a5afdcc2d5dddc52b41d746000b77983fd88b026
Gery Ruslandi <geryruslandi@gmail.com>
2025-11-01 13:44:41 +0700

    wip hyprland config integration

--- flake.nix
+++ flake.nix
@@ -4,12 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, ... }@inputs:
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; }; # this is the important part
         modules = [
           ./configuration.nix
         ];
