d6f381ff2a400f9c8f8f28e9d01c9415dccd48f3
Gery Ruslandi <geryruslandi@gmail.com>
2025-11-01 11:57:07 +0700

    initial working kde

---
+++ modules/users.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.geryruslandi = {
+    isNormalUser = true;
+    description = "Gery Ruslandi";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      kdePackages.kate
+      vscode
+      nixfmt-rfc-style
+      #  thunderbird
+    ];
+  };
+}
