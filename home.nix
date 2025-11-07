8cc9bf666a050e9575b5fedc13fc35358a121052
Gery Ruslandi <geryruslandi@gmail.com>
2025-11-07 18:10:26 +0700

    finalized illogical impulse

--- home.nix
+++ home.nix
@@ -8,15 +8,12 @@
   # home.nix
   imports = [
     ./homes/zen-browser.nix
-    ./homes/hyprland.nix
-    ./homes/kitty.nix
-    ./homes/dolphin.nix
     ./homes/illogical-impulse.nix
   ];
 
   home = {
     packages = with pkgs; [
-      hello
+      nwg-displays
     ];
 
     # This needs to actually be set to your username
