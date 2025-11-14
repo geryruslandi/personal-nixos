1f44534acf2649c031731be77522975b14b66573
Gery Ruslandi <geryruslandi@gmail.com>
2025-11-15 01:03:52 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "template for hydenix";
+
+  inputs = rec {
+    # Your nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Hydenix
+    hydenix = {
+      # Available inputs:
+      # Main: github:richen604/hydenix
+      # Commit: github:richen604/hydenix/<commit-hash>
+      # Version: github:richen604/hydenix/v1.0.0 - note the version may not be compatible with this template
+      url = "github:richen604/hydenix";
+
+      # uncomment the below if you know what you're doing, hydenix updates nixos-unstable every week or so
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Hardware Configuration's, used in ./configuration.nix. Feel free to remove if unused
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+  };
+
+  outputs =
+    { ... }@inputs:
+    let
+      system = "x86_64-linux";
+      hydenixConfig = inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./configuration.nix
+        ];
+      };
+
+    in
+    {
+      nixosConfigurations.hydenix = hydenixConfig;
+      nixosConfigurations.default = hydenixConfig;
+    };
+}
