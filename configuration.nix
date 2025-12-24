2638c61922c2b67c5298f991af476181e8ef5bfd
Gery Ruslandi <geryruslandi@gmail.com>
2025-12-24 19:16:16 +0700

    revert to systemd boot

--- configuration.nix
+++ configuration.nix
@@ -22,10 +22,9 @@
   xdg.mime.enable = true;
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
