7d7ad57c2d96ace4c82cb13c438af6111b4bc849
Gery Ruslandi <geryruslandi@gmail.com>
2025-12-27 17:58:36 +0700

    fix default browser and app menu entry on dolphin

--- configuration.nix
+++ configuration.nix
@@ -44,8 +44,28 @@
     "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
   };
 
-  # Ensures system-level environment variables point to the flatpak
-  environment.sessionVariables.DEFAULT_BROWSER = "flatpak run app.zen_browser.zen";
+  environment.sessionVariables = {
+    DEFAULT_BROWSER = "flatpak run app.zen_browser.zen";
+    XDG_MENU_PREFIX = "plasma-";
+    QT_QPA_PLATFORMTHEME = "kde"; # Or "qt6ct" if you use that for styling
+  };
+
+  environment.etc."xdg/menus/applications.menu".source =
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
+  # Ensure portals are enabled for Hyprland
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.kdePackages.xdg-desktop-portal-kde
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+    # Recommended: specify which portal to use for certain tasks
+    config.common.default = [
+      "hyprland"
+      "kde"
+    ];
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -5,5 +5,9 @@
     git
     fastfetch
     dbeaver-bin
+    # For dolphin open app menu fixes
+    shared-mime-info
+    kdePackages.kservice
+    kdePackages.kio-extras
   ];
 }
