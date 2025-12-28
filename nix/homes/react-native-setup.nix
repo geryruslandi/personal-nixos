a558406398e3ed4facdc972210aff7dc1f4ce6ad
Gery Ruslandi <geryruslandi@gmail.com>
2025-12-28 12:37:40 +0700

    fix missing jdk declaration on react native setup

--- nix/homes/react-native-setup.nix
+++ nix/homes/react-native-setup.nix
@@ -6,10 +6,18 @@
 {
   home.packages = with pkgs; [
     android-studio
+    jdk17
   ];
+
+  # Manages the Java environment for this user
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk17; # You can specify pkgs.jdk17, pkgs.jdk23, etc.
+  };
 
   home.sessionVariables = {
     ANDROID_HOME = "$HOME/Android/Sdk";
+    JAVA_HOME = "${pkgs.jdk17}/lib/openjdk";
   };
 
   home.sessionPath = [
