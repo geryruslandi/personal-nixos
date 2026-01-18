d99b2d95649b7b7fb0d100efed2d3c24782ff205
Gery Ruslandi <geryruslandi@gmail.com>
2026-01-18 21:14:18 +0700

    php with memory limit set

--- nix/homes/php.nix
+++ nix/homes/php.nix
@@ -1,27 +1,37 @@
 { pkgs, ... }:
 let
   # Define a PHP build with all Laravel mandatory extensions
-  phpWithLaravel = pkgs.php82.withExtensions ({ enabled, all }:
-    enabled ++ (with all; [
-      bcmath
-      ctype
-      curl
-      dom
-      fileinfo
-      filter
-      gd
-      intl
-      mbstring
-      openssl
-      pdo
-      pdo_mysql # Change to pdo_pgsql or pdo_sqlite as needed
-      session
-      tokenizer
-      xml
-      xmlwriter
-      zip
-    ])
-  );
+  phpWithLaravel = pkgs.php82.buildEnv {
+    # Specify your extensions here (same format as withExtensions)
+    extensions =
+      { all, enabled }:
+      with all;
+      enabled
+      ++ [
+        bcmath
+        ctype
+        curl
+        dom
+        fileinfo
+        filter
+        gd
+        intl
+        mbstring
+        openssl
+        pdo
+        pdo_mysql # Change to pdo_pgsql or pdo_sqlite as needed
+        session
+        tokenizer
+        xml
+        xmlwriter
+        zip
+      ];
+
+    # Add your custom memory limit here
+    extraConfig = ''
+      memory_limit = 2G
+    '';
+  };
 in
 {
   home.packages = [
