66642ac263da69b6a83d532b8d647831eeda5e11
Gery Ruslandi <geryruslandi@gmail.com>
2026-07-02 01:00:33 +0700

    add some of required ld-library for python

--- nix/homes/python.nix
+++ nix/homes/python.nix
@@ -14,6 +14,10 @@
   '';
 
   programs.zsh.initExtra = ''
+    export LD_LIBRARY_PATH="${lib.makeLibraryPath (with pkgs; [
+      stdenv.cc.cc
+      zlib
+    ])}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
     source "$HOME/.virtualenvs/global/bin/activate"
   '';
 }
