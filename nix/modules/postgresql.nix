059c1fc21bf06f537618fb8c17c196031e0f547c
Gery Ruslandi <geryruslandi@gmail.com>
2026-07-06 15:54:28 +0700

    add ip6 local access on postgre

--- nix/modules/postgresql.nix
+++ nix/modules/postgresql.nix
@@ -14,6 +14,7 @@ in
       #type database  DBuser  auth-method
       local all       all     trust
       host  all       all     127.0.0.1/32  trust
+      host  all       all     ::1/128       trust
     '';
 
     ensureUsers = lib.optional pg.enable {
